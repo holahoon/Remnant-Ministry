3cfed5217eda64f883f6a7f28dfc859f279235c2
David Kim <rtdk0324@gmail.com>
2019-09-22 18:07:19 -0400

    main page update

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,29 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
 
 import ArrowRight20 from "@carbon/icons-react/es/arrow--right/20";
 
 import "./MainPage.css";
 
-const mainPage = props => {
-  return (
-    <div className={"Content"}>
-      <div className={"Header Col-4 margin-left-2"}>
-        <div className={"white-line"} />
+class MainPage extends Component {
+  render() {
+    const viewEvent = (
+      <NavLink to="/Event-details" className={"ViewEventButton"}>
+        View event here
+        <ArrowRight20 className={"ArrowIcon"} />
+      </NavLink>
+    );
 
-        <div>
-          <h2>2019</h2>
-          <h3 className={"margin-left-1"}>Nov 28-30 Chicago</h3>
+    const loginToViewEvent = (
+      <NavLink to="/login" className={"ViewEventButton"}>
+        Login to view event
+        <ArrowRight20 className={"ArrowIcon"} />
+      </NavLink>
+    );
+
+    return (
+      <div className={"Content"}>
+        <div className={"Header Col-4 margin-left-2"}>
+          <div className={"white-line"} />
+
+          <div>
+            <h2>2019</h2>
+            <h3 className={"margin-left-1"}>Nov 28-30 Chicago</h3>
+          </div>
+
+          <h1>Remnant Conference of America</h1>
+
+          {this.props.loginToken ? viewEvent : loginToViewEvent}
         </div>
-
-        <h1>Remnant Conference of America</h1>
-
-        <NavLink to="/Event-details" className={"ViewEventButton"}>
-          View event here <ArrowRight20 className={"ArrowIcon"} />
-        </NavLink>
       </div>
-    </div>
-  );
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    loginToken: state.globalLogin.token !== null
+  };
 };
 
-export default mainPage;
+export default connect(mapStateToProps)(MainPage);
